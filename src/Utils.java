36c7754e255c30338465bd8328f512629a9e78ba
Michael Wang <michaelwang9000@gmail.com>
2019-03-07 01:00:31 -0800

    use removeExtraPunctuation method in parse Two016ElectionResults method (my two key is broken)

--- src/Utils.java
+++ src/Utils.java
@@ -22,26 +22,26 @@ public class Utils {
 
     public static ArrayList<ElectionResult> parse2016ElelectionResults(String data) {
         ArrayList<ElectionResult> results = new ArrayList<>();
-        String[] electionResults = data.split("\n");
-        for(int i = 1; i < electionResults.length; i++) {
-            String[] vals = data.split(", ");
-            double votes_dem = Double.parseDouble(vals[1]);
-            double votes_gop = Double.parseDouble(vals[2]);
-            double total_votes = Double.parseDouble(vals[3]);
-            double per_dem = Double.parseDouble(vals[4]);
-            double per_gop = Double.parseDouble(vals[5]);
-            double diff = Double.parseDouble(vals[6].substring(1, vals[6].length() - 1));
-            double per_point_diff = Double.parseDouble(vals[7].substring(0, vals[7].length() - 1)) / 100;
-            String state_abbr = vals[8];
-            String county_name = vals[9];
-            int combined_fips = Integer.parseInt(vals[10]);
+        String[] initial = data.split(", ");
+        ArrayList<String> vals = removeExtraPunctuation(initial);
+        for(int i = 1; i < vals.size(); i++) {
+            double votes_dem = Double.parseDouble(vals.get(1));
+            double votes_gop = Double.parseDouble(vals.get(2));
+            double total_votes = Double.parseDouble(vals.get(3));
+            double per_dem = Double.parseDouble(vals.get(4));
+            double per_gop = Double.parseDouble(vals.get(5));
+            double diff = Double.parseDouble(vals.get(6));
+            double per_point_diff = Double.parseDouble(vals.get(7)) / 100;
+            String state_abbr = vals.get(8);
+            String county_name = vals.get(9);
+            int combined_fips = Integer.parseInt(vals.get(10));
             ElectionResult result = new ElectionResult(votes_dem, votes_gop, total_votes, per_dem,per_gop,diff,per_point_diff,state_abbr,county_name,combined_fips);
             results.add(result);
         }
         return  results;
     }
 
-    private ArrayList<String> removeExtraPunctuation(String[] initial) {
+    private static ArrayList<String> removeExtraPunctuation(String[] initial) {
         ArrayList<String> output = new ArrayList<>();
         for(String value:initial){
             if(value.contains(",")) value = value.replace(",", "");
